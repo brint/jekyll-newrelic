d5e82bc643102b8d28961a0f568260c735fb2c0f
Brint O'Hearn <brint.ohearn@rackspace.com>
2016-05-15 15:04:16 -0500

    Adding codeclimate test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+if ENV['CI']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 require 'jekyll-newrelic'
 
 require 'minitest/autorun'
